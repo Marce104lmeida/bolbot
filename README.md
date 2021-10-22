f006e4200996d8fc6c8c13b3c4f8987595a9eda3
Marce104lmeida <marcelo.almeida1989@bol.com.brgit config --global user.email marcelo.almeida1989@bol.com.brgit config --global user.email marcelo.almeida1989@bol.com.br>
2021-10-22 16:24:12 -0300

    origin

---
+++ README.md
@@ -0,0 +1,13 @@
+Esté é um projeto desenvolvido em python e selenium webdriver.
+
+O objetivo do código é abrir o navegador google chrome e loga no email fornecido;
+Após será aberto o primeiro email e encaminhado para um email também fornecido;
+Após encaminhado o email será deletado!
+E este processo será repetido até que não haja mais emails a ser encaminhado;
+Até o momento o código faz o processo bem com os emails da caixa de entrada;
+
+				****ainda a ser implementado***
+
+1 - imprementar o envio dos email da caixa de spam
+2 - o código ainda precisa de intervenção humana pois algumas vezes ao logar no email precisa 'acertar' o captha.
+
---
+++ app.py
@@ -0,0 +1,61 @@
+from selenium import webdriver, common
+import time
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
+# Autor: Marcelo Oliveira Almeida
+# Email: marcelo.almeida1989@bol.com.br
+# este bot funcionou com o email da bol
+# futuramente será adicionado para funcionar com mais provedores
+driver = webdriver.Chrome('C:\\Users\MARCELO\\anaconda3\\chromedriver.exe')
+driver.get('https://bmail.uol.com.br/?xc=26264ffb5c818fe37ae1766d03e2b735#/webmail/0//INBOX/page:1')
+driver.maximize_window()
+# email onde esta os email a serem encaminhados!
+EMAIL_ADRESS =''
+#senha do email!
+EMAIL_PASSWORD = ''
+# email para onde será encaminhado os emails
+enc_para = ''
+total_envio = 0
+time.sleep(2)
+user_field = driver.find_element(By.ID, 'user')
+time.sleep(1)
+user_senha = driver.find_element(By.ID, 'pass')
+btn_entrar = driver.find_element(By.ID, 'button-submit')
+user_senha.send_keys(EMAIL_PASSWORD)
+user_field.send_keys(EMAIL_ADRESS)
+time.sleep(1)
+btn_entrar.click()
+time.sleep(20)
+existe = False
+try:
+    selected_email = driver.find_element(By.XPATH,'/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[1]/div/div[5]/ul/li[1]/a/div/div[3]/span')
+    time.sleep(2)
+    selected_email.click()
+except common.exceptions.NoSuchElementException:
+    print("Falha no Login ou / nenhum email nesta pasta")
+else:
+    print('')
+time.sleep(5)
+for email in range(101):
+    try:
+        encaminhar = driver.find_element(By.XPATH, '/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[2]/div/div[1]/div[1]/menu[3]/span[2]')
+        encaminhar.click()
+        time.sleep(3)
+        destino = driver.find_element(By.ID, 'fake_input__field-to')
+        destino.send_keys(enc_para)
+        time.sleep(5)
+        btn_enviar = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[1]/form/div[4]/menu[1]/span[2]')
+        btn_enviar.click()
+        time.sleep(2)
+        btn_apagar = driver.find_element(By.XPATH, '/html/body/div[3]/div/section[2]/div/div[2]/div/div[1]/div[1]/span/ul/li[2]/span/span')
+        btn_apagar.click()
+        time.sleep(3)
+        total_envio +=1
+    except common.exceptions.NoSuchElementException:
+        back_entrada = driver.find_element(By.XPATH, "//*[@id='fc-collapsable-sidebar']/div[3]/div[2]/div/div[1]/ul/li[1]")
+        back_entrada.click()
+        break
+    else:
+        print('')
+print(f'Foram enviados um total de: {total_envio} email(s)')
