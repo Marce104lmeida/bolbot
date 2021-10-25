10506bf49dcebbbfc4599c6e36f3a10de6cf8cf3
Marce104lmeida <marcelo.almeida1989@bol.com.brgit config --global user.email marcelo.almeida1989@bol.com.brgit config --global user.email marcelo.almeida1989@bol.com.br>
2021-10-25 12:05:53 -0300

    criada as funçoes de encaminhar e apagar

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ from selenium.webdriver.common.keys import Keys
 # este bot funcionou com o email da bol
 # futuramente será adicionado para funcionar com mais provedores
 driver = webdriver.Chrome('C:\\Users\MARCELO\\anaconda3\\chromedriver.exe')
-driver.get('https://bmail.uol.com.br/?xc=26264ffb5c818fe37ae1766d03e2b735#/webmail/0//INBOX/page:1')
+driver.get('https://conta.uol.com.br/login?t=bol&env=visitante&dest=https://bmail.uol.com.br/login/check_session')
 driver.maximize_window()
 # email onde esta os email a serem encaminhados!
-EMAIL_ADRESS =''
+EMAIL_ADRESS ='marcelo.almeida1989@bol.com.br'
 #senha do email!
-EMAIL_PASSWORD = ''
+EMAIL_PASSWORD = '151289a'
 # email para onde será encaminhado os emails
-enc_para = ''
-total_envio = 0
+enc_para = '4systemweb@gmail.com'
+
 time.sleep(2)
 user_field = driver.find_element(By.ID, 'user')
 time.sleep(1)
@@ -27,35 +27,73 @@ user_field.send_keys(EMAIL_ADRESS)
 time.sleep(1)
 btn_entrar.click()
 time.sleep(20)
-existe = False
-try:
-    selected_email = driver.find_element(By.XPATH,'/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[1]/div/div[5]/ul/li[1]/a/div/div[3]/span')
-    time.sleep(2)
-    selected_email.click()
-except common.exceptions.NoSuchElementException:
-    print("Falha no Login ou / nenhum email nesta pasta")
-else:
-    print('')
-time.sleep(5)
-for email in range(101):
+
+# Criada a função encaminhar
+def encaminhar():
+    total_envio = 0
     try:
-        encaminhar = driver.find_element(By.XPATH, '/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[2]/div/div[1]/div[1]/menu[3]/span[2]')
-        encaminhar.click()
-        time.sleep(3)
-        destino = driver.find_element(By.ID, 'fake_input__field-to')
-        destino.send_keys(enc_para)
-        time.sleep(5)
-        btn_enviar = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[1]/form/div[4]/menu[1]/span[2]')
-        btn_enviar.click()
+        selected_email = driver.find_element(By.XPATH,
+                                             '/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[1]/div/div[5]/ul/li[1]/a/div/div[3]/span')
         time.sleep(2)
-        btn_apagar = driver.find_element(By.XPATH, '/html/body/div[3]/div/section[2]/div/div[2]/div/div[1]/div[1]/span/ul/li[2]/span/span')
-        btn_apagar.click()
-        time.sleep(3)
-        total_envio +=1
+        selected_email.click()
     except common.exceptions.NoSuchElementException:
-        back_entrada = driver.find_element(By.XPATH, "//*[@id='fc-collapsable-sidebar']/div[3]/div[2]/div/div[1]/ul/li[1]")
-        back_entrada.click()
-        break
+        print("Falha no Login ou / nenhum email nesta pasta")
     else:
         print('')
-print(f'Foram enviados um total de: {total_envio} email(s)')
+    time.sleep(5)
+    for email in range(101):
+        try:
+            encaminhar = driver.find_element(By.XPATH,
+                                             '/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[2]/div/div[1]/div[1]/menu[3]/span[2]')
+            encaminhar.click()
+            time.sleep(3)
+            destino = driver.find_element(By.ID, 'fake_input__field-to')
+            destino.send_keys(enc_para)
+            time.sleep(5)
+            btn_enviar = driver.find_element(By.XPATH,
+                                             '/html/body/div[3]/div/div[1]/div[1]/form/div[4]/menu[1]/span[2]')
+            btn_enviar.click()
+            time.sleep(2)
+            btn_apagar = driver.find_element(By.XPATH,
+                                             '/html/body/div[3]/div/section[2]/div/div[2]/div/div[1]/div[1]/span/ul/li[2]/span/span')
+            btn_apagar.click()
+            time.sleep(3)
+            total_envio += 1
+        except common.exceptions.NoSuchElementException:
+            back_entrada = driver.find_element(By.XPATH,
+                                               "//*[@id='fc-collapsable-sidebar']/div[3]/div[2]/div/div[1]/ul/li[1]")
+            back_entrada.click()
+            break
+        else:
+            print('')
+
+        print(f'Foram enviados um total de: {total_envio} email(s)')
+
+
+def lixeira():
+    lixeira = driver.find_element(By.XPATH, "//*[@id='fc-collapsable-sidebar']/div[3]/div[2]/div/div[1]/ul/li[4]")
+    lixeira.click()
+    time.sleep(3)
+    for i in range(1000):
+        try:
+            chkbox = driver.find_element(By.XPATH, "/html/body/div[3]/div/section[2]/div/div[2]/div/div[2]/section[1]/div/div[1]/label")
+            time.sleep(3)
+            chkbox.click()
+            time.sleep(1)
+            btn_apagar = driver.find_element(By.XPATH, "/html/body/div[3]/div/section[2]/div/div[2]/div/div[1]/div[1]/span/ul/li[2]/span")
+            time.sleep(1)
+            btn_apagar.click()
+            time.sleep(1)
+            confirma = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[2]/div/div/span[1]")
+            time.sleep(1)
+            confirma.click()
+            time.sleep(2)
+        except:
+         break
+        else:
+           print("")
+
+encaminhar()
+time.sleep(1)
+lixeira()
+
